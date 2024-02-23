81697cba05ec2eaecb767fbeabd64f8d9db1a119
saozd <saozdemir1991@gmail.com>
2024-02-24 02:53:11 +0300

    Initial commit

---
+++ sample/src/SampleMain.java
@@ -0,0 +1,12 @@
+/**
+ * @author saozd
+ * @project PACKAGE_NAME javaeeworkspace
+ * @date 24 Feb 2024
+ * <p>
+ * @description:
+ */
+public class SampleMain {
+    public static void main(String[] args) {
+        System.out.println("Sample");
+    }
+}
